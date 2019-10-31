9e10604f1ba5d45d8504cf158a5acc4b7c6d6ec8
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-10-31 08:41:08 -0500

    compile markdown to html

--- src/__tests__/lib.js
+++ src/__tests__/lib.js
@@ -1,13 +1,18 @@
-import plugin from '../lib';
+import plugin, { compile } from '../lib';
 import { CONFIG } from '@parcel/plugin';
 
 describe('plugin export interface', () => {
   it('exposes the correct methods', () => {
     expect(plugin[CONFIG]).toMatchObject({
-      getConfig: expect.any(Function),
-      parse: expect.any(Function),
-      transform: expect.any(Function),
-      generate: expect.any(Function)
+      // getConfig: expect.any(Function),
+      // parse: expect.any(Function),
+      transform: expect.any(Function)
+      // generate: expect.any(Function)
     });
+  });
+
+  it('compiles markdown to html', async () => {
+    const html = await compile('## hello world');
+    expect(html).toContain('<h2>hello world</h2>');
   });
 });
